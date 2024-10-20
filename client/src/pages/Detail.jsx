502a4fe8bf78646231e23927938ae7906a4c7e2b
meryem-uludagli <meryemuslu2035@gmail.com>
2024-10-20 17:58:43 +0300

    addee

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,18 +1,19 @@
-import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { Link, useParams } from "react-router-dom";
 import { IoMdArrowRoundBack } from "react-icons/io";
-import { FaClock } from "react-icons/fa";
-import { PiForkKnifeFill } from "react-icons/pi";
 import api from "../api";
+import { MdEdit } from "react-icons/md";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
+import { FaClock } from "react-icons/fa";
+import { PiForkKnifeFill } from "react-icons/pi";
 import DeleteButton from "../components/DeleteButton";
-import { MdEdit } from "react-icons/md";
 
 const Detail = () => {
   const { id } = useParams();
+
   const { isLoading, error, data, refetch } = useQuery({
-    queryKey: ["recipe"],
+    queryKey: ["recipe", id],
     queryFn: () =>
       api.get(`/api/v1/recipes/${id}`).then((res) => res.data.found),
   });
@@ -20,22 +21,21 @@ const Detail = () => {
   return (
     <div className="max-w-2xl mx-auto">
       <div className="flex items-center  justify-between mb-10">
-        <Link to={-1} className="btn flex items-center gap-2 py-1">
+        <Link to={-1} className="flex items-center gap-2 py-1">
           <IoMdArrowRoundBack />
           Geri
         </Link>
 
-        <div className="flex items-center gap-2">
-          <Link
-            className="btn flex gap-2 items-center bg-blue-500 hover:bg-blue-600 py-1 min-w-[80px] justify-center"
-            to={`/düzenle/${data?.id}`}
-          >
-            <MdEdit />
-            Düzenle
-          </Link>
+        <Link
+          className="flex gap-2 items-center bg-blue-500 hover:bg-blue-600 py-1 min-w-[80px] justify-center py-2 px-4 rounded-md text-white font-semibold text-lg"
+          to={`/düzenle/${data?.id}`}
+        >
+          {" "}
+          <MdEdit />
+          Düzenle
+        </Link>
 
-          <DeleteButton disabled={!data?.id} productId={data?.id} />
-        </div>
+        <DeleteButton disabled={!data?.id} productId={data?.id} />
       </div>
 
       {isLoading ? (
@@ -45,44 +45,44 @@ const Detail = () => {
       ) : (
         data && (
           <div>
-            <h1 className="title text-3xl">{data.recipeName}</h1>
+            <h1 className="text-3xl font-bold text-red-400">
+              {data.recipeName}
+            </h1>
 
             <div className="flex gap-4 my-5">
-              <span className="badge">
-                <PiForkKnifeFill />
+              <span className="flex items-center justify-center bg-yellow-500 py-2 px-4 rounded-lg text-white font-semibold">
+                <PiForkKnifeFill className="mr-2" />
                 {data.category}
               </span>
-              <span className="badge">
-                <FaClock />
-                {data.recipeTime} dak.
+              <span className="flex items-center justify-center bg-yellow-500 py-2 px-4 rounded-lg text-white font-semibold">
+                <FaClock className="mr-2" />
+                {data.recipeTime}dk
               </span>
             </div>
 
             <img
-              className="rounded-lg max-h-[350px] w-full object-cover"
+              className="rounded-lg max-h-[400] w-full object-cover"
               src={data.image}
               alt={data.recipeName}
             />
 
             <div className="my-5">
-              <h2 className="title">Malzemeler</h2>
-
+              <h2 className="text-3xl font-bold text-red-400">Malzemeler</h2>
               <ul>
-                {data.ingredients.map((i, key) => (
-                  <li key={key} className="font-semibold text-lg">
-                    {i}
+                {data.ingredients.map((ingredient, index) => (
+                  <li key={index} className="font-semibold text-xl">
+                    {ingredient}
                   </li>
                 ))}
               </ul>
             </div>
 
             <div className="my-5">
-              <h2 className="title">Tarif</h2>
-
+              <h2 className="text-3xl font-bold text-red-400">Tarif</h2>
               <ol className="list-decimal ps-4">
-                {data.instructions.map((i, key) => (
-                  <li key={key} className="font-semibold text-lg">
-                    {i}
+                {data.instructions.map((instruction, index) => (
+                  <li key={index} className="font-semibold text-xl">
+                    {instruction}
                   </li>
                 ))}
               </ol>
@@ -90,8 +90,9 @@ const Detail = () => {
 
             {data.servingSuggestion && (
               <div className="my-5">
-                <h2 className="title">Sunum Önerisi</h2>
-
+                <h2 className="text-3xl font-bold text-red-400">
+                  Sunum Önerisi
+                </h2>
                 <p className="text-lg font-semibold">
                   {data.servingSuggestion}
                 </p>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,34 +5,38 @@ import Search from "../components/Search";
 import Sort from "../components/Sort";
 import { useState } from "react";
 import { useDebounce } from "@uidotdev/usehooks";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
 
 const Home = () => {
+  const [order, setOrder] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   const debouncedTerm = useDebounce(searchTerm, 500);
-  const [order, setOrder] = useState(null);
 
   const params = {
     order,
     search: debouncedTerm,
   };
-
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: ["recipes", order, debouncedTerm],
     queryFn: () =>
       api.get("/api/v1/recipes", { params }).then((res) => res.data.recipes),
   });
+
   return (
     <main className="overflow-y-auto">
       <Search setSearchTerm={setSearchTerm} />
+
       <section>
         {isLoading ? (
-          "Loader"
+          <Loader />
         ) : error ? (
-          "Error"
+          <Error info={error.message} refetch={refetch} />
         ) : (
           <>
             <div className="flex justify-between items-center">
-              <h1 className="text-3xl my-5">{data.length}tarif bulundu</h1>
+              <h1 className="text-3xl my-5">{data.length} tarif bulundu</h1>
+
               <Sort setOrder={setOrder} />
             </div>
 
